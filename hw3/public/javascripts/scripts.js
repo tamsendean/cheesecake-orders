9399fa40f5c60e297c52813bfd9986ebe555d54a
tamsendean <deant23@up.edu>
2022-01-31 17:56:32 -0800

    up to pt7

---
+++ hw3/public/javascripts/scripts.js
@@ -0,0 +1,21 @@
+<><script>
+    var t = $("#topping").val();
+    var q = $("#quantity").val();
+</script><script>
+        $(document).ready(function(){$("form").submit(function () {
+            var str = $("#notes").val();
+            var v = "vegan";
+            if (str.indexOf(v) != -1) {
+                alert("Warning: Cheesecakes contain dairy.");
+            } else {
+                $("form").replaceWith("Thank you! Your order has been placed.  Topping: " + t + " Quantity: " + q + " Notes: " + str);
+                $("table").remove();
+            }
+        })};
+    </script><script>
+    $(document).ready(function(){
+    $( "a" ).click(function() {
+      var monthval = $(this).attr("value");
+      $("#dropbut").text(monthval);
+    })};
+</script></>
